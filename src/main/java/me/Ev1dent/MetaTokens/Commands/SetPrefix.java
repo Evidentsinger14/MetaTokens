3942611c7724c22a2fe69f013f62b05af76039c6
Evidentsinger14 <cdelancey186423@gmail.com>
2021-10-08 22:00:05 -0400

    remove stupid log replace filter for testing

--- src/main/java/me/Ev1dent/MetaTokens/Commands/SetPrefix.java
+++ src/main/java/me/Ev1dent/MetaTokens/Commands/SetPrefix.java
@@ -14,14 +14,17 @@ public class SetPrefix implements CommandExecutor {
             if (args.length == 0) {
                 player.sendMessage("Usage: /SetPrefix CustomPrefix");
             }
-            String UMPrefix = args[0];
-            String MPrefix = UMPrefix.replace("&0", "").replace("&1", "").replace("&2", "").replace("&3", "").replace("&4", "").replace("&5", "").replace("&6", "").replace("&7", "").replace("&8", "").replace("&9", "").replace("&a", "").replace("&b", "").replace("&c", "").replace("&d", "").replace("&e", "").replace("&f", "").replace("&l", "").replace("&k", "").replace("&n", "").replace("&m", "").replace("&o", "").replace("&r", "").replace("[", "").replace("]", "");
-            String newPrefix = UMPrefix.replace("&k", "").replace("[", "").replace("]", "");
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', newPrefix));
-            if (MPrefix.length() > 10 || !MPrefix.matches("[a-zA-Z]")) {
+            String RawPrefix = args[0];
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', RawPrefix));
+            if (RawPrefix.length() > 10 || !RawPrefix.matches("[a-zA-Z]")) {
                 player.sendMessage("Your prefix must be less than 10 characters long, cannot contain non-alphanumeric characters.");
             }
         }
         return false;
     }
 }
+
+
+//String MPrefix = UMPrefix.replace("&0", "").replace("&1", "").replace("&2", "").replace("&3", "").replace("&4", "").replace("&5", "").replace("&6", "").replace("&7", "").replace("&8", "").replace("&9", "").replace("&a", "").replace("&b", "").replace("&c", "").replace("&d", "").replace("&e", "").replace("&f", "").replace("&l", "").replace("&k", "").replace("&n", "").replace("&m", "").replace("&o", "").replace("&r", "").replace("[", "").replace("]", "");
+//            String newPrefix = UMPrefix.replace("&k", "").replace("[", "").replace("]", "");
+//

bd9249b709e44e1e233aa124556f67f282d87dbb
Evidentsinger14 <cdelancey186423@gmail.com>
2021-10-09 21:42:31 -0400

    :)

--- src/main/java/me/Ev1dent/MetaTokens/Commands/SetPrefix.java
+++ src/main/java/me/Ev1dent/MetaTokens/Commands/SetPrefix.java
@@ -1,10 +1,13 @@
 package me.Ev1dent.MetaTokens.Commands;
 
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SetPrefix implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -13,17 +16,24 @@ public class SetPrefix implements CommandExecutor {
         if (label.equalsIgnoreCase("setprefix")) {
             if (args.length < 1) {
                 //returns a usage message if no args are specified
+//                int tokenamount = 0;
                 player.sendMessage("Usage: /SetPrefix CustomPrefix");
                 return true;
             }
+            //Bandit is a fucking dumbass
             String RawPrefix = args[0];
-            //check if the tag has alphanumeric characters (non 0-9 & a-z)
-            if (RawPrefix.matches("[a-zA-Z]")) {
-                player.sendMessage("Your prefix works!");
+            //start regex check
+            Pattern pattern = Pattern.compile("[a-zA-Z0-9]*");
+            //(?i)[§&][0-9A-FL-ORX]
+            Matcher match = pattern.matcher(RawPrefix);
+            //checks if regex is met
+            if (!match.matches()) {
+                player.sendMessage("Your prefix doesn't work!");
+                return true;
             }
-            player.sendMessage("Your prefix Doesn't work!");
-            return true;
+            player.sendMessage("Your prefix works!");
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', RawPrefix));
         }
-        return false;
+        return true;
     }
 }

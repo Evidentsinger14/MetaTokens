89285ef8d2ea3fe8f23bb518efbcfa92a1117731
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-03-11 16:12:03 -0500

    add space

--- src/main/java/dev/Ev1dent/MetaTokens/MTMain.java
+++ src/main/java/dev/Ev1dent/MetaTokens/MTMain.java
@@ -79,7 +79,7 @@ public class MTMain extends JavaPlugin {
             // adds the node
             user.data().add(node);
             player.sendMessage("You have successfully received the prefix: " + prefix);
-            player.sendMessage("You have " + MTMain.plugin.getTokens(player) + "Tokens left.");
+            player.sendMessage("You have " + MTMain.plugin.getTokens(player) + " Tokens left.");
         });
     }
 
@@ -94,7 +94,7 @@ public class MTMain extends JavaPlugin {
             // adds the node
             user.data().add(node);
             player.sendMessage("You have successfully received the suffix: " + suffix);
-            player.sendMessage("You have " + MTMain.plugin.getTokens(player) + "Tokens left.");
+            player.sendMessage("You have " + MTMain.plugin.getTokens(player) + " Tokens left.");
         });
     }
 

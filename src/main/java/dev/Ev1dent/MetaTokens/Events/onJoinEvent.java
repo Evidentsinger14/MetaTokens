8543ffec1bf6c5018f0def8e43ecf50765bd2e9d
Evidentsinger14 <Cdelancey186423@gmail.com>
2023-03-11 05:43:33 -0500

    fix player join event

--- src/main/java/dev/Ev1dent/MetaTokens/Events/onJoinEvent.java
+++ src/main/java/dev/Ev1dent/MetaTokens/Events/onJoinEvent.java
@@ -1,19 +1,21 @@
 package dev.Ev1dent.MetaTokens.Events;
 
-import dev.Ev1dent.MetaTokens.MTMain;
 import dev.Ev1dent.MetaTokens.Utilities.Utils;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class onJoinEvent implements Listener {
     Utils Utils = new Utils();
-    private MTMain plugin;
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
-            if(plugin.getConfig().getInt("tokens") > 0){
-                e.getPlayer().sendMessage(Utils.Color("&aWelcome " + e.getPlayer().getName() + "!" ));
-            }
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        if (!player.hasPlayedBefore()) {
+            player.sendMessage("Welcome to the server, " + player);
+        } else {
+            player.sendMessage("Welcome back, " + player);
+        }
     }
 }
